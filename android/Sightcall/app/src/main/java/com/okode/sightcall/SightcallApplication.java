28671cc1f20589610fd3d32ff2abeeb2b8c19ca5
Rubén Panadero <rpanadero@okode.com>
2019-02-12 09:35:39 +0100

    Configured sightcall application

---
+++ android/Sightcall/app/src/main/java/com/okode/sightcall/SightcallApplication.java
@@ -0,0 +1,44 @@
+package com.okode.sightcall;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.sightcall.universal.Universal;
+import com.sightcall.universal.event.CallReportEvent;
+import com.sightcall.universal.media.MediaSavedEvent;
+import com.sightcall.universal.scenario.Scenario;
+
+import net.rtccloud.sdk.Call;
+import net.rtccloud.sdk.event.Event;
+
+import static com.okode.sightcall.Constants.*;
+
+public class SightcallApplication extends Application {
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Universal.register(this);
+    }
+
+    @Event
+    public void onScenarioStatusEvent(Scenario.StatusEvent event) {
+        Log.i(TAG, event.status().toString());
+    }
+
+    @Event
+    public void onCallStatusEvent(Call event) {
+        Log.i(TAG, event.status().toString());
+    }
+
+    @Event
+    public void onCallFinished(CallReportEvent event) {
+        Log.i(TAG, event.toString());
+    }
+
+    @Event
+    public void onMediaSavedEvent(MediaSavedEvent event) {
+        Log.i(TAG, event.toString());
+    }
+
+}
